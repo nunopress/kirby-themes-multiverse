8e3a0d83bda6c7ac05e11bdd09065a61d214cb6a
Pereira Ricardo <hello@nunopress.com>
2016-04-29 23:45:27 +0200

    Fixed issue for sending email.
    Thanks for issue reporting @jagaland

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -11,9 +11,15 @@ return function ($site, $pages, $page, $args) {
         return;
     }
 
-    mail($site->email(), sprintf("Contact form site by %s", $query['name']), $query['message'], sprintf("From: <%s> %s", $query['email'], $query['name']));
+    //mail($site->email(), sprintf("Contact form site by %s", $query['name']), $query['message'], sprintf("From: <%s> %s", $query['email'], $query['name']));
 
-    return [
-        'contact_form_success' => true
+    $email = email([
+        'to' => $site->email(),
+        'from' => $query['email'],
+        'subject' => sprintf("Contact form site by %s <%s>", $query['name'], $query['email']),
+        'body' => $query['message']
+    ]);
+
+    return ($email->send()) ? [ 'contact_form_success' => true ] : [ 'contact_form_error' => $email->error() ]
     ];
 };
